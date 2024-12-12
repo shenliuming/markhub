8aafe882117a2d389f7af8fad30dbd1fb07f8834
shenliuming <ceekayshen@foxmail.com>
2024-12-12 17:57:05 +0800

    markhub-framework add UniversalMetaObjectHandler

---
+++ markhub-framework/src/main/java/com/markhub/framework/util/FrameworkUtils.java
@@ -0,0 +1,24 @@
+package com.markhub.framework.util;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+/**
+ * @Auther: emp
+ * @Date: 2024/12/12 17:47
+ * @Description:
+ */
+public class FrameworkUtils {
+
+
+    public static Long getLoginUserId(){
+        return 0L;
+    }
+
+    public static Long getLoginUserId(HttpServletRequest request) {
+        if (request == null) {
+            return null;
+        }
+        return 0L;
+    }
+
+}
